aeddd95171738678603712e43eca39cf96ab2148
Vintage Coder <gowthamanvai@gmail.com>
2021-12-16 17:51:59 +0530

    Control template initial commit
    control template was committed

---
+++ Templates Example/Templates Example/ControlTemplate.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Templates_Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Home Button was Clicked");
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Home Button was Clicked");
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("About Button was Clicked");
+        }
+    }
+}
